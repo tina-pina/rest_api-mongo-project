96fbcef6591fd172bccbf10efa739dface4902b7
tina-pina <christina.koenig.de@gmail.com>
2019-05-05 10:53:10 +0200

    Add Cors package

--- app.js
+++ app.js
@@ -6,31 +6,43 @@ var app = express();
 var routes = require("./routes");
 var logger = require('morgan');
 var jsonParser = require("body-parser").json;
+var cors = require('cors')
+
 
 // variable to enable global error logging
 const enableGlobalErrorLogging = process.env.ENABLE_GLOBAL_ERROR_LOGGING === 'true';
 
+
 // setup morgan which gives us http request logging
 app.use(logger("dev"));
 app.use(jsonParser());
+
+
+// to allow request among different origins/domains -> cors npm package 
+app.use(cors())
+
 
 // mongoose
 var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost:27017/fsjstd-restapi", { useNewUrlParser: true });
 var db = mongoose.connection;
 
+
 //if there's an error connecting to the database
 db.on("error", function (err) {
   console.error("connection error:", err);
 })
+
 
 //if successful
 db.once("open", function () {
   console.log("db connection successful")
 })
 
+
 // API
 app.use("/api", routes);
+
 
 // setup a friendly greeting for the root route
 app.get('/', (req, res) => {
@@ -39,12 +51,14 @@ app.get('/', (req, res) => {
   });
 });
 
+
 // send 404 if no other route matched
 app.use((req, res) => {
   res.status(404).json({
     message: 'Route Not Found',
   });
 });
+
 
 // setup a global error handler
 app.use((err, req, res, next) => {
@@ -58,8 +72,10 @@ app.use((err, req, res, next) => {
   });
 });
 
+
 // set our port
 app.set('port', process.env.PORT || 5000);
+
 
 // start listening on our port
 const server = app.listen(app.get('port'), () => {
