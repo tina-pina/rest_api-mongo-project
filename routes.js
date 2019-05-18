ff97a597970a0d0c6079ae12d12cf177fb49afd7
tina-pina <christina.koenig.de@gmail.com>
2019-05-18 20:38:22 +0200

    Update response code

--- routes.js
+++ routes.js
@@ -101,12 +101,10 @@ router.post('/users', [
         password: bcryptjs.hashSync(req.body.password),
     });
     user.save().then(result => {
-        console.log(result);
-        res.location('/api');
-        res.status(201).json(result);
+        res.location('/');
+        res.status(201).end();
     })
         .catch(err => {
-            console.log(err);
             res.status(400).json({ error: err });
         });
 });
@@ -154,8 +152,8 @@ router.post("/courses", [
         .exists({ checkNull: true, checkFalsy: true })
         .withMessage('Please provide a value for "description"')
 ], function (req, res, next) {
-    // validate title or description
 
+    // validate title or description
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
@@ -180,7 +178,8 @@ router.post("/courses", [
             if (err) next(err);
             // document was successfully saved 
             else {
-                res.status(201).json(course)
+                res.location = `/api/courses/${course._id}`
+                res.status(201).end()
             };
         })
     }
@@ -199,44 +198,33 @@ router.put("/courses/:ID", [
 ], function (req, res, next) {
 
     const errors = validationResult(req);
-    // {errors: [....]}
 
     if (!errors.isEmpty()) {
         // Use the Array `map()` method to get a list of error messages.
         const errorMessages = errors.array().map(error => error.msg);
-
         // Return the validation errors to the client.
         res.status(400).json({ errors: errorMessages });
     }
     else {
 
-        // console.log("current course", req.course.user)
         let courseOwnerId = req.course.user
         let currUserId = req.currentUser._id;
 
         if (currUserId.equals(courseOwnerId)) {
             // update
             req.course.update(req.body, function (err, result) {
-
                 if (err) {
-                    console.log(err)
-                    //res.status(400).json(err)
                     next(err);
                 }
                 //send results in question document back to client
                 else {
-                    console.log("sent")
-                    res.status(200).json(result);
+                    res.status(204).end();
                 }
-
             });
         } else {
             // unauthorized
             res.status(401).json({ "errors": ["You are not allowed to update other user's courses"] });
         }
-
-
-
     }
 })
 
@@ -250,7 +238,7 @@ router.delete("/courses/:ID", [authenticateUser], function (req, res) {
         //remove method of mongoose
         req.course.remove(function (err) {
             if (err) next(err);
-            else res.status(200).json({ "ok": 1 });
+            else res.status(204).end()
         });
     }
     else {
